23fc0ee52e98d4f3cec52f64e15e4481d29ec118
SK <68364467+eunmin-kim@users.noreply.github.com>
2024-05-15 12:25:59 +0900

    [Silver II] Title: 예산, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -2,26 +2,27 @@ n = int(input())
 jibang = list(map(int, input().split()))
 budget = int(input())
 
-def cal_budget(mid):
+def cal_mid(mid):
     total = 0
     for i in jibang:
-        if i > mid:
-            total += mid
-        else:
+        if i <= mid:
             total += i
+        else:
+            total += mid
     return total
 
 
-min_n = 0
-max_n = max(jibang)
+low = 0
+high = max(jibang)
 answer = -1
 
-while min_n <= max_n:
-    mid = (max_n + min_n) // 2
+while low <= high:
+    mid = (low + high) // 2
     
-    if cal_budget(mid) <= budget:
+    if cal_mid(mid) <= budget:
         answer = mid
-        min_n = mid + 1
+        low = mid + 1
     else:
-        max_n = mid - 1
+        high = mid - 1
+
 print(answer)
